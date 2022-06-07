71fc96653a31c85a3f5088c8b3fb4415603dae25
gaga5lala <gaga5lala1013@gmail.com>
2022-06-07 21:29:27 +0800

    test redis

--- pkg/api/main.go
+++ pkg/api/main.go
@@ -1,13 +1,20 @@
 package api
 
 import (
+	"context"
 	"dcard-pretest/pkg/store"
+	"encoding/json"
 	"net/http"
 	"strconv"
 
+	"github.com/go-redis/redis/v9"
 	logger "github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	leaderboardKey = "dcard-leaderboard"
 )
 
 // TODO: type Score = store.Score
@@ -55,9 +62,42 @@ func main() {
 		c.JSON(http.StatusOK, scores)
 	})
 
+	v1.GET("/redis-leaderboard", func(c *gin.Context) {
+		redisClient := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		})
+		scores, err := redisClient.ZRevRangeWithScores(c, leaderboardKey, 0, 9).Result()
+		if err != nil {
+			logger.Errorln(err)
+		}
+		c.JSON(http.StatusOK, scores)
+	})
+
+	v1.POST("/redis-score", func(c *gin.Context) {
+		redisClient := redis.NewClient(&redis.Options{
+			Addr:     "localhost:6379",
+			Password: "",
+			DB:       0,
+		})
+		params := map[string]interface{}{}
+		clientID := c.GetHeader("ClientId")
+		params["client_id"] = clientID
+		err = json.NewDecoder(c.Request.Body).Decode(&params)
+
+		_, err = addScore(redisClient, params)
+		if err != nil {
+			logger.Infoln(err)
+		}
+		c.JSON(http.StatusAccepted, gin.H{
+			"resp": "hihi",
+		})
+	})
+
 	// TODO: create record
 	v1.POST("/score", func(c *gin.Context) {
-		clientID := c.Request.Header["ClientId"]
+		clientID := c.GetHeader("ClientId")
 
 		c.JSON(http.StatusAccepted, gin.H{
 			"client_id": clientID,
@@ -65,4 +105,39 @@ func main() {
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func addScore(c *redis.Client, p map[string]interface{}) (map[string]interface{}, error) {
+
+	ctx := context.TODO()
+
+	// clientId := p["clientId"].(string)
+	clientId := p["client_id"].(string)
+	score := p["score"].(float64)
+
+	//Validate data here in a production environment
+
+	err := c.ZAdd(ctx, leaderboardKey, redis.Z{
+		Score:  score,
+		Member: clientId,
+	}).Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	rank := c.ZRank(ctx, leaderboardKey, clientId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := map[string]interface{}{
+		"data": map[string]interface{}{
+			"nickname": p["nickname"].(string),
+			"rank":     rank.Val(),
+		},
+	}
+
+	return response, nil
 }
